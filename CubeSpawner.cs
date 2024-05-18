7498935e7cdae7e725993803f5a62f29479f2f65
Diamond2536 <117559386+Diamond2536@users.noreply.github.com>
2024-05-18 07:51:57 +0300

    Add files via upload

--- CubeSpawner.cs
+++ CubeSpawner.cs
@@ -1,27 +1,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class CubeSpawner : MonoBehaviour
+public class CubeSpawner : MonoBehaviour
 {
-    [SerializeField] protected Cube _cubeTemplate;
+    [SerializeField] private Cube _cubeTemplate;
+    [SerializeField] private int _minCubesQuantity = 2;
+    [SerializeField] private int _maxCubesQuantity = 7;
+    [SerializeField] private bool _spawnOnStart = false;
+    [SerializeField] private bool _fragmentOnSpawn = false;
+    [SerializeField] private float _scaleFactor = 0.5f;
 
-    [SerializeField] int _minCubesQuantity = 2;
-    [SerializeField] int _maxCubesQuantity = 7;
-
-    private int GetCubesQuantity()
+    private void Start()
     {
-        return Random.Range(_minCubesQuantity, _maxCubesQuantity);
+        if (_spawnOnStart)
+        {
+            SpawnCubes(transform.position);
+        }
+    }    
+
+    public void SetFragmentOnSpawn(bool value)
+    {
+        _fragmentOnSpawn = value;
     }
 
-    private Color RandomizeCubesColor()
+    public void SetScaleFactor(float value)
     {
-        return Random.ColorHSV();
+        _scaleFactor = value;
     }
 
     public List<Cube> SpawnCubes(Vector3 position)
     {
         List<Cube> spawnedCubes = new List<Cube>();
-
         int cubesAmount = GetCubesQuantity();
 
         for (int i = 0; i < cubesAmount; i++)
@@ -33,12 +42,28 @@ public abstract class CubeSpawner : MonoBehaviour
         return spawnedCubes;
     }
 
-    protected virtual Cube SpawnCube(Vector3 position)
+    private int GetCubesQuantity()
+    {
+        return Random.Range(_minCubesQuantity, _maxCubesQuantity);
+    }
+
+    private Color RandomizeCubesColor()
+    {
+        return Random.ColorHSV();
+    }
+
+    private Cube SpawnCube(Vector3 position)
     {
         Color color = RandomizeCubesColor();
         Cube cube = Instantiate(_cubeTemplate, position, Quaternion.identity);
         Renderer cubeRenderer = cube.GetComponent<Renderer>();
         cubeRenderer.material.color = color;
+
+        if (_fragmentOnSpawn)
+        {
+            cube.transform.localScale *= _scaleFactor;
+        }
+
         return cube;
-    }
+    }    
 }
--- Detonator.cs
+++ Detonator.cs
@@ -3,43 +3,46 @@ using UnityEngine;
 
 public class Detonator : MonoBehaviour
 {
-    private Camera _mainCamera;
-    private int _leftMouseButton = 0;
-
-    [SerializeField] private FragmentCubeSpawner _fragmentCubeSpawner;
+    [SerializeField] private CubeSpawner _cubeSpawner;
     [SerializeField] private float _radius;
     [SerializeField] private float _explosionForce;
 
+    private Camera _mainCamera;
+
+    private int _leftMouseButton = 0;
+
     private float _splitChance = 1f;
     private float _splitFactor = 0.5f;
+    private float _scaleFactor = 0.5f;
 
     private void Start()
     {
         _mainCamera = Camera.main;
-        _fragmentCubeSpawner = FindObjectOfType<FragmentCubeSpawner>();
+
+        if (_cubeSpawner != null)
+        {
+            _cubeSpawner.SetFragmentOnSpawn(true);
+            _cubeSpawner.SetScaleFactor(_scaleFactor);
+        }
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(_leftMouseButton))
         {
-            RaycastHit hit;
             Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Cube hitCube;
-
-                if (hit.collider.TryGetComponent(out hitCube))
+                if (hit.collider.TryGetComponent(out Cube hitCube))
                 {
                     if (Random.value <= _splitChance)
                     {
                         Vector3 hitCubePosition = hitCube.transform.position;
-                        Explode(hitCubePosition, _fragmentCubeSpawner.SpawnFragmentedCubes(hitCubePosition));
+                        Explode(hitCubePosition, _cubeSpawner.SpawnCubes(hitCubePosition));
                         Destroy(hitCube.gameObject);
                         _splitChance *= _splitFactor;
                     }
-
                     else
                     {
                         Destroy(hitCube.gameObject);
@@ -51,19 +54,11 @@ public class Detonator : MonoBehaviour
 
     private void Explode(Vector3 center, List<Cube> cubesToExplode)
     {
-        Collider[] colliders = Physics.OverlapSphere(center, _radius);
-
-        foreach (Collider hit in colliders)
+        foreach (Cube cube in cubesToExplode)
         {
-            if (hit.TryGetComponent(out Rigidbody rigidBody))
+            if (cube.TryGetComponent(out Rigidbody rigidBody))
             {
-                foreach (Cube cube in cubesToExplode)
-                {
-                    if (hit.gameObject == cube.gameObject)
-                    {
-                        rigidBody.AddExplosionForce(_explosionForce, center, _radius);
-                    }
-                }
+                rigidBody.AddExplosionForce(_explosionForce, center, _radius);
             }
         }
     }
